51039f4416ab7d657c3d1b105f43bcc8b9234dd0
SlowWave <chrs.borgia@gmail.com>
2023-04-06 17:18:59 +0200

    new features added

--- commands.js
+++ commands.js
@@ -32,7 +32,7 @@ function TwoBodyCircle1() {
   var vel1 = [0, 0, 0];
   var vel2 = [0, v, 0];
 
-  scenario.CreateBody(pos1, vel1, 7, m1, null);
+  scenario.CreateBody(pos1, vel1, 10, m1, null);
   scenario.CreateBody(pos2, vel2, 2, m2, null);
 }
 
@@ -46,13 +46,15 @@ function TwoBodyCircle2() {
   var pos2 = [-pos1[0], 0, 0];
 
   var res = ComputeDistance(pos1, pos2);
-  var v = Math.sqrt(m1 * g_const / res.distance_norm) * Math.sqrt(2) / 2;
+  // var v = Math.sqrt(m1 * g_const / res.distance_norm) * Math.sqrt(2) / 2;
+  
+  var v = Math.sqrt(1/2 * m1 * g_const / res.distance_norm);
 
   var vel1 = [0, v, 0];
   var vel2 = [0, -v, 0];
 
-  scenario.CreateBody(pos1, vel1, 3, m1, null);
-  scenario.CreateBody(pos2, vel2, 3, m2, null);
+  scenario.CreateBody(pos1, vel1, 4, m1, null);
+  scenario.CreateBody(pos2, vel2, 4, m2, null);
 }
 
 function TwoBodyElliptic1() {
@@ -64,7 +66,7 @@ function TwoBodyElliptic1() {
   var pos1 = [0, 0, 0];
   var pos2 = [getRandomInt(50, 150), 0, 0];
 
-  var ell = getRandomInt(4, 9);
+  var ell = getRandomInt(5, 8);
 
   var res = ComputeDistance(pos1, pos2);
   var v = Math.sqrt(m1 * g_const / res.distance_norm) * ell / 10;
@@ -72,7 +74,7 @@ function TwoBodyElliptic1() {
   var vel1 = [0, 0, 0];
   var vel2 = [0, v, 0];
 
-  scenario.CreateBody(pos1, vel1, 7, m1, null);
+  scenario.CreateBody(pos1, vel1, 10, m1, null);
   scenario.CreateBody(pos2, vel2, 2, m2, null);
 }
 
@@ -86,7 +88,7 @@ function TwoBodyElliptic2() {
   var pos1 = [getRandomInt(50, 150), 0, 0];
   var pos2 = [-pos1[0], 0, 0];
 
-  var ell = getRandomInt(3, 9);
+  var ell = getRandomInt(3, 6);
 
   var res = ComputeDistance(pos1, pos2);
   var v = Math.sqrt(m1 * g_const / res.distance_norm) * ell / 10;
@@ -94,15 +96,40 @@ function TwoBodyElliptic2() {
   var vel1 = [0, v, 0];
   var vel2 = [0, -v, 0];
 
-  scenario.CreateBody(pos1, vel1, 3, m1, null);
-  scenario.CreateBody(pos2, vel2, 3, m2, null);
+  scenario.CreateBody(pos1, vel1, 4, m1, null);
+  scenario.CreateBody(pos2, vel2, 4, m2, null);
 }
 
 
+function ThreeBodyCircle1s() {
+  scenario.ClearScenario();
+
+  var m1 = getRandomInt(5000, 10000);
+  var m2 = m1/10000;
+  var m3 = m1/10000;
+
+  
+  var pos1 = [0, 0, 0];
+  var pos2 = [getRandomInt(50, 150), 0, 0];
+  var pos3 = [-pos2[0], 0, 0];
+
+  // var ell = getRandomInt(3, 9);
+
+  var res12 = ComputeDistance(pos1, pos2);
+  var res23 = ComputeDistance(pos2, pos3);
+  var v = Math.sqrt(g_const * res12.distance_norm * (m1 / Math.pow(res12.distance_norm, 2) + m2 / Math.pow(res23.distance_norm, 2)));
+
+  var vel1 = [0, 0, 0];
+  var vel2 = [0, v, 0];
+  var vel3 = [0, -v, 0];
+
+  scenario.CreateBody(pos1, vel1, 10, m1, null);
+  scenario.CreateBody(pos2, vel2, 2, m2, null);
+  scenario.CreateBody(pos3, vel3, 2, m3, null);
+}
 
 
-
-function ThreeBody1() {
+function ThreeBodyCircle1ua() {
   scenario.ClearScenario();
 
   var m1 = getRandomInt(5000, 10000);
@@ -118,13 +145,78 @@ function ThreeBody1() {
 
   var res12 = ComputeDistance(pos1, pos2);
   var res23 = ComputeDistance(pos2, pos3);
-  var v = Math.sqrt(m1 * g_const * res12.distance_norm * (1 / Math.pow(res12.distance_norm, 2) + 1 / Math.pow(res23.distance_norm, 2)));
+  var v = Math.sqrt(g_const * res12.distance_norm * (m1 / Math.pow(res12.distance_norm, 2) + m2 / Math.pow(res23.distance_norm, 2)));
 
   var vel1 = [0, 0, 0];
-  var vel2 = [0, -v, 0];
-  var vel3 = [0, v, 0];
+  var vel2 = [0, v, 0];
+  var vel3 = [0, -v, 0];
 
-  scenario.CreateBody(pos1, vel1, 3, m1, null);
-  scenario.CreateBody(pos2, vel2, 3, m2, null);
-  scenario.CreateBody(pos3, vel3, 3, m3, null);
+  scenario.CreateBody(pos1, vel1, 4, m1, null);
+  scenario.CreateBody(pos2, vel2, 4, m2, null);
+  scenario.CreateBody(pos3, vel3, 4, m3, null);
 }
+
+
+function ThreeBodyCircle1ub() {
+  scenario.ClearScenario();
+
+  var m1 =  getRandomInt(5000, 10000);
+  var m2 = m1;
+  var m3 = m1;
+
+  var dist_h = getRandomInt(50, 150);
+
+  var ang1 = getRandomInt(0, 360);
+  var ang2 = ang1 + 120;
+  var ang3 = ang2 + 120;
+
+  var ang1 = Math.PI / 180 * ang1;
+  var ang2 = Math.PI / 180 * ang2;
+  var ang3 = Math.PI / 180 * ang3;
+
+
+  var pos1 = [Math.sin(ang1) * dist_h, Math.cos(ang1) * dist_h, 0];
+  var pos2 = [Math.sin(ang2) * dist_h, Math.cos(ang2) * dist_h, 0];
+  var pos3 = [Math.sin(ang3) * dist_h, Math.cos(ang3) * dist_h, 0];
+
+  var v = Math.sqrt(1/2 * m1 * g_const / (dist_h * (Math.sqrt(3)/2)));
+
+  // var v = Math.sqrt(g_const * dist_h * (m1 / Math.pow(dist_h, 2) + m2 / Math.pow(dist_h, 2)));
+
+  var vel1 = [Math.sin(ang1 - Math.PI/2) * v, Math.cos(ang1 - Math.PI/2) * v, 0];
+  var vel2 = [Math.sin(ang2 - Math.PI/2) * v, Math.cos(ang2 - Math.PI/2) * v, 0];
+  var vel3 = [Math.sin(ang3 - Math.PI/2) * v, Math.cos(ang3 - Math.PI/2) * v, 0];
+
+  scenario.CreateBody(pos1, vel1, 4, m1, null);
+  scenario.CreateBody(pos2, vel2, 4, m2, null);
+  scenario.CreateBody(pos3, vel3, 4, m3, null);
+}
+
+
+
+function ThreeBodyFigureEight() {
+  scenario.ClearScenario();
+
+  var m1 =  getRandomInt(5000, 10000);
+  var m2 = m1;
+  var m3 = m1;
+
+  var ang = Math.PI / 180 * 30;
+
+  var pos1 = [0, 0, 0];
+  var pos2 = [getRandomInt(50, 150), 0, 0];
+  var pos3 = [-pos2[0], 0, 0];
+
+  var dist = pos2[0];
+
+  var v = Math.sqrt(m1 * g_const * dist * (1 / Math.pow(dist, 2) + 1 / Math.pow(dist * 2, 2))) / 1.8;
+
+  var vel1 = [- Math.sin(ang) * 2 * v,- Math.cos(ang) * 2 * v, 0];
+  var vel2 = [Math.sin(ang) * v, Math.cos(ang) * v, 0];
+  var vel3 = [Math.sin(ang) * v, Math.cos(ang) * v, 0];
+
+  scenario.CreateBody(pos1, vel1, 4, m1, null);
+  scenario.CreateBody(pos2, vel2, 4, m2, null);
+  scenario.CreateBody(pos3, vel3, 4, m3, null);
+}
+
